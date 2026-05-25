b8e2e4ea2f6bb4e419a569be52b870a2eb00f66c
tienanaa <tienthao261@gmail.com>
2026-05-25 14:35:30 +0700

    add chat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,36 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Login from '../Component/login/login';
-import Home from  '../Component/home/home';
-import Lesson from '../Component/Lesson/lesson'
-import ContentLesson from '../Component/ContentLesson/contentLesson'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "../Component/login/login";
+import Home from "../Component/home/home";
+import Lesson from "../Component/Lesson/lesson";
+import ContentLesson from "../Component/ContentLesson/contentLesson";
+import ChatBox from "../Component/ChatBox/ChatBox";
+
 function App() {
-
   return (
-    <Routes>
-      {/* 🎯 Đường dẫn mặc định (/) sẽ hiển thị trang Login */}
-      <Route 
-        path="/" 
-        element={
-          <div id="center">
-            <Login />
-          </div>
-        } 
-      />
+    // Thêm thẻ mở <> ở đây
+    <>
+      <Routes>
+        {/* 🎯 Đường dẫn mặc định (/) sẽ hiển thị trang Login */}
+        <Route
+          path="/"
+          element={
+            <div id="center">
+              <Login />
+            </div>
+          }
+        />
 
-      {/* 🎯 Đường dẫn (/home) sẽ hiển thị trang tiếp theo */}
-       <Route path="/home" element={<Home />} /> 
-        <Route path="/lesson" element={<Lesson />} /> 
-        <Route path='/ContentLesson' element={<ContentLesson/>} />
-    </Routes>
-  )
+        {/* 🎯 Đường dẫn (/home) sẽ hiển thị trang tiếp theo */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/lesson" element={<Lesson />} />
+        <Route path="/ContentLesson" element={<ContentLesson />} />
+      </Routes>
+
+      {/* 🎯 NHÉT CHATBOX VÀO ĐÂY (Nằm ngoài Routes nhưng nằm trong thẻ <>) */}
+      <ChatBox />
+    </> // Thêm thẻ đóng </> ở đây
+  );
 }
 
-export default App
+export default App;
